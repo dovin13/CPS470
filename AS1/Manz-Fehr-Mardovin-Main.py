cfcb78c0d6df57fd1879ba3d65c63dfb817bce36
dovin13 <imardovin542@gmail.com>
2022-09-12 23:19:02 -0400

    updated with uniqueness check not confirmed working

--- AS1/Manz-Fehr-Mardovin-Main.py
+++ AS1/Manz-Fehr-Mardovin-Main.py
@@ -1,10 +1,22 @@
 
 from AS1socket import TCPsocket
 from AS1request import Request
-
+import socket
 import time
 from urllib.parse import urlparse
 
+
+
+def getnewIP(hostname):
+        
+        if (len(hostname) > 64):  # socket fails with idna codec error when a host name exceeds 64 characters.
+            return None
+        try:
+            ip = socket.gethostbyname(hostname)   # ip is a local variable to getIP(hostname), ip is of string type
+        except socket.gaierror:
+            print("Failed to gethostbyname")
+            return None
+        return ip
 
 def main(): # function, method are the same
 
@@ -13,7 +25,8 @@ def main(): # function, method are the same
  #numThreads = input('How many threads to run: ')
  #inputfile = input('What is the input file: ')
 
-
+ checkIP = []
+ checkHost = []
  with open("URL-input-100.txt","r") as f:
   
   for line in f:
@@ -28,12 +41,27 @@ def main(): # function, method are the same
     port  = 80
     print('Parsing URL... host ' + parsed.hostname + ', port ' + str(port) + ' request /' + parsed.query)   
     start = time.perf_counter()
-    ip = mysocket.getIP(parsed.hostname) # ip is a local variable to getIP(hostname), ip is of string type
+
+
+    ip = mysocket.getIP(parsed.hostname)      # ip is a local variable to getIP(hostname), ip is of string type
+
+    checkHost.append(parsed.hostname)
+    checkIP.append(ip)
+
+
+    if(checkHost.count(parsed.hostname) == 1):
+        print('Checking host uniqueness... passed')
+    else:
+        print('Checking host uniqueness... failed')
     end = time.perf_counter()
     timme = (end-start) * 1000
     timme = int(timme)
     print("Doing DNS... done in ",str(timme)," ms, found ", ip)
     start2 = time.perf_counter() #connection timer 
+    if(checkIP.count(ip) == 1):
+        print('Checking IP uniqueness... passed')
+    else:
+        print('Checking IP uniqueness... failed')
     mysocket.connect(ip, port)
     timme = (time.perf_counter() - start2) * 1000
     timme = int(timme)
@@ -44,11 +72,7 @@ def main(): # function, method are the same
     #working on what is passed into this
     msg = myrequest.headRequest(parsed.hostname)
     print(msg.decode())
-   
-
-
     # send out request
-
     mysocket.send(msg)
     start = time.perf_counter()
     data = mysocket.receive()
@@ -57,20 +81,16 @@ def main(): # function, method are the same
     tim = int(tim)
     print('Loading... done in '+ str(tim) + ' ms'  + ' with ' + str(len(data)) + ' bytes')
     string = data.decode()
-
     print('Verifying header... status code ' + str(string[9: 12]))
     
-
     print('_' * 120)
-   #where looping needs to end (end point)
 
     print(data.decode())
-
     msg1 = myrequest.getRequest(parsed.hostname, parsed.path, parsed.query)
     mysocket.send(msg1)
     dat = mysocket.receive()
     #the get request does not send back info the HEAD request does and logic is up above
-    print('getRequest: ' + dat.decode())
+    #print('getRequest: ' + dat.decode())
     print('_' *120)
     mysocket.close()
     time.sleep(1)
