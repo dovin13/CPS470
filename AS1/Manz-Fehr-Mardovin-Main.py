6d7b02534dfb613eacba3159695157da1bdf211f
dovin13 <imardovin542@gmail.com>
2022-09-13 12:45:13 -0400

    updated code implementing thread

--- AS1/Manz-Fehr-Mardovin-Main.py
+++ AS1/Manz-Fehr-Mardovin-Main.py
@@ -22,10 +22,70 @@ class thread(threading.Thread):
       while Q:
         threadLock.acquire()
         #call to all the logic for the main program 
-        
-        thread.uniqueLogic()
+        thread.Logic()
         threadLock.release()
+    def Logic():
+      if(len(Q) == 0):
+            return ''
+      if(len(Q) != 0):
+    #Parts that need to be looped (starting point)
+        mysocket = TCPsocket() # create an object of TCP socket
+        mysocket.createSocket()
+        if(len(Q) == 0):
+            return ''
+        host = Q.pop(0)
+        print('URL: ' + host)
+        parsed = urlparse(host) #parses url to get specific things from the URL itself
+        port  = 80
+        print('Parsing URL... host ' + parsed.hostname + ', port ' + str(port) + ' request /' + parsed.query)   
+        start = time.perf_counter()
+        ip = mysocket.getIP(parsed.hostname)      # ip is a local variable to getIP(hostname), ip is of string type
+        if(checkHost.count(parsed.hostname) == 1):
+            print('Checking host uniqueness... passed')
+        else:
+            print('Checking host uniqueness... failed')
+        end = time.perf_counter()
+        timme = (end-start) * 1000
+        timme = int(timme)
+        print("Doing DNS... done in ",str(timme)," ms, found ", ip)
+        start2 = time.perf_counter() #connection timer 
+        if(checkIP.count(ip) == 1 and ip != None):
+            print('Checking IP uniqueness... passed')
+        else:
+            print('Checking IP uniqueness... failed')
+        mysocket.connect(ip, port)
+        timme = (time.perf_counter() - start2) * 1000
+        timme = int(timme)
+        print('Connecting on page... done in ' + str(timme) + ' ms')
 
+    # build our request
+        myrequest = Request()
+    #working on what is passed into this
+        msg = myrequest.headRequest(parsed.hostname)
+   # print(msg.decode())
+    # send out request
+        mysocket.send(msg)
+        start = time.perf_counter()
+        data = mysocket.receive()
+        end = time.perf_counter()
+        tim = (end - start) * 1000
+        tim = int(tim)
+        print('Loading... done in '+ str(tim) + ' ms'  + ' with ' + str(len(data)) + ' bytes')
+        string = data.decode()
+        print('Verifying header... status code ' + str(string[9: 12]))
+    
+        print('_' * 120)
+
+        print(data.decode())
+        msg1 = myrequest.getRequest(parsed.hostname, parsed.path, parsed.query)
+        mysocket.send(msg1)
+        dat = mysocket.receive()
+    #the get request does not send back info the HEAD request does and logic is up above
+    #print('getRequest: ' + dat.decode())
+        print('_' *120)
+        mysocket.close()
+        return ''
+   # time.sleep(1)
 def getnewIP(hostname):
         if (len(hostname) > 64):  # socket fails with idna codec error when a host name exceeds 64 characters.
             return None
@@ -41,8 +101,9 @@ def getnewIP(hostname):
 if __name__ == "__main__":
 
 
- #numThreads = input('How many threads to run: ')
- #inputfile = input('What is the input file: ')
+ #https://stackoverflow.com/questions/6181935/how-do-you-create-different-variable-names-while-in-a-loop
+ #use link above to create multiple threads in a loop after input of how many threads needed
+
  urlArray = []
  global checkIP
  global checkHost
@@ -62,12 +123,17 @@ if __name__ == "__main__":
     Q = Queue()
     Q = urlArray
     print(checkIP)
- #end of future main method when thread is implemented
 
- #put everything below in a function that is called inside the thread run(self) method    
+
+ thread1 = thread(1)
+ thread1.start()
+ thread1.join()
+
+
+
  with open("URL-input-100.txt","r") as f:
   
-  for line in f:
+   for line in f:
     #Parts that need to be looped (starting point)
     mysocket = TCPsocket() # create an object of TCP socket
     mysocket.createSocket()
@@ -102,7 +168,7 @@ if __name__ == "__main__":
     myrequest = Request()
     #working on what is passed into this
     msg = myrequest.headRequest(parsed.hostname)
-    print(msg.decode())
+   # print(msg.decode())
     # send out request
     mysocket.send(msg)
     start = time.perf_counter()
@@ -124,7 +190,7 @@ if __name__ == "__main__":
     #print('getRequest: ' + dat.decode())
     print('_' *120)
     mysocket.close()
-    time.sleep(1)
+   # time.sleep(1)
 
 
 
