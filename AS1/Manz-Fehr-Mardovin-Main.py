64fa2db786b728a91b8c86b29abd98eb7d7e9cb6
dovin13 <imardovin542@gmail.com>
2022-09-29 11:54:21 -0400

    changes to outputs

--- AS1/Manz-Fehr-Mardovin-Main.py
+++ AS1/Manz-Fehr-Mardovin-Main.py
@@ -10,6 +10,11 @@ from queue import Queue
 threadLock = threading.Lock()
 blank = 0
 counter = 0
+hostcounter = 0
+ipunique = 0
+httpcode = []
+httpcodecounter = 0
+
 
 class thread(threading.Thread):
 
@@ -19,24 +24,29 @@ class thread(threading.Thread):
         # helper function to execute the threads
     def run(self):
      
-      while Q:
-        print(len(Q))
+      while Q:  
         threadLock.acquire()
         #call to all the logic for the main program 
         thread.Logic(self.thread_ID)
         threadLock.release()
         if(len(Q) == 0):
+            print('hello')
             thread(self.thread_ID).do_run = False
     def Logic(id):
       #set global here initialization above this class
       global blank
       global counter
+      global httpcode
+      global httpcodecounter
+      global hostcounter
+      global ipunique
+
       if(len(Q) == 0):
             return ''
       if(id == 0):
         blank = blank + 2
         active = threading.active_count() - 1
-        print("time: " + str(blank) + " active: " + str(active))
+        print("[ " + str(blank) + "]   " + str(active) + " Q   " + str(len(Q)) + " E   " + str(hostcounter) + " H   " + str(ipunique) + " I   " + " R   " + str(httpcodecounter) + " C   " + " L   " + "K")
         time.sleep(2)
         return ''
       else:
@@ -54,12 +64,14 @@ class thread(threading.Thread):
         port  = 80
         ip = mysocket.getIP(parsed.hostname)      # ip is a local variable to getIP(hostname), ip is of string type
         if(checkHost.count(parsed.hostname) == 1):
+            hostcounter = hostcounter + 1
             print('Checking host uniqueness... passed')
         else:
             print('Checking host uniqueness... failed')
 
    
         if(checkIP.count(ip) == 1 and ip != None):
+            ipunique = ipunique + 1
             print('Checking IP uniqueness... passed')
         else:
             print('Checking IP uniqueness... failed')
@@ -69,9 +81,12 @@ class thread(threading.Thread):
         msg1 = myrequest.getRequest(parsed.hostname, parsed.path, parsed.query)
         mysocket.send(msg1)
         dat = mysocket.receive()
-        print('Verifying header... status code ' + str(dat[9: 12].decode()))
+        httpcode.append(dat[9:12].decode())
+        for code in httpcode:
+            if(len(code) != 0):
+                httpcodecounter = httpcodecounter + 1
 
-        #print('getRequest: ' + dat.decode())
+        print('Verifying header... status code ' + str(dat[9: 12].decode()))
         print('_' *120)
         mysocket.close()
         return ''
@@ -90,7 +105,7 @@ def getnewIP(hostname):
         return ip
 
 if __name__ == "__main__":
-
+ start = time.perf_counter()
  #for part 3
  #https://stackoverflow.com/questions/6181935/how-do-you-create-different-variable-names-while-in-a-loop
  #use link above to create multiple threads in a loop after input of how many threads needed
@@ -98,17 +113,16 @@ if __name__ == "__main__":
  
  global checkIP
  global checkHost
- global pendingqueue
- global extracted
- global successDNS
- global uniqueIP
- global uniqueHost
  global robotcheck
- global HTTPcode
- 
+
+ code2 = 0
+ code3 = 0
+ code4 = 0
+ code5 = 0
+ codeother = 0
  
  urlArray = []
- HTTPcode = []
+
  checkIP = []
  checkHost = []
  
@@ -152,8 +166,24 @@ if __name__ == "__main__":
  thread4.join()
 
 
+ end = time.perf_counter()
+
+ print("Extracted " + str(counter) + " @ " + str(counter / (end - start)) + "/s")
+ for codes in httpcode:
+  if(codes != ''):
+    if(codes[0] == '2'):
+        code2 = code2 + 1
+    if(codes[0] == '3'):
+        code3 = code3 + 1
+    if(codes[0] == '4'):
+        code4 = code4 + 1
+    if(codes[0] == '5'):
+        code5 = code5 + 1
+  if(codes == ''):
+    codeother = codeother + 1
+
  
- print('hello')
+ print('HTTP codes: 2xx = ' + str(code2) + ", 3xx = " + str(code3) + ", 4xx = " + str(code4) + ", 5xx = " + str(code5) + ", other = " + str(codeother))
 
 
 
